5735d12e0987f837a1e440fa12da23f854956f6b
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-01-21 10:25:28 +0200

    Create framework project and adding default files

---
+++ NetworkLayer/NetworkLayer/NetworkLayer.h
@@ -0,0 +1,18 @@
+//
+//  NetworkLayer.h
+//  NetworkLayer
+//
+//  Created by Mohammed Abd El-Aty on 21/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkLayer.
+FOUNDATION_EXPORT double NetworkLayerVersionNumber;
+
+//! Project version string for NetworkLayer.
+FOUNDATION_EXPORT const unsigned char NetworkLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkLayer/PublicHeader.h>
+
+
